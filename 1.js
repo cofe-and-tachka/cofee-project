da8331c72f9e2590cf52dca5bba36de2ff67359b
Анна <166999455+AnnaMerlica@users.noreply.github.com>
2025-04-19 19:16:20 +0300

    Update 1.js
    скрипты для главной страницы

--- 1.js
+++ 1.js
@@ -1,1 +1,30 @@
+// Скрипт для карусели
+let slideIndex = 1;
+showSlides(slideIndex);
 
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.querySelectorAll('.carousel img');
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('active');
+    }
+    slides[slideIndex - 1].classList.add('active');
+}
+
+// Функции для кнопок
+function prevSlide() {
+    plusSlides(-1);
+}
+
+function nextSlide() {
+    plusSlides(1);
+}
